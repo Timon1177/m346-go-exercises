cca8c8122866a4c0517c7cfe022377bd2ade798b
TIMON SOOM <TIMON_SOOM@sluz.ch>
2024-11-22 15:54:51 +0100

    Ex 1

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
+var firstName, lastName string = "Timon", "Soom"
+var dayOfBirth, monthOfBirth, yearOfBirth, numberOfSiblings int = 21, 03, 2008, 3
+var heightInMeters float32 = 1.85
+var zodiacSign rune = '\u2648'
+
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
